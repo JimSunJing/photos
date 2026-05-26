5cee42bbd345309ea137976f114d268b84f4b66f
JimS <jimsun6428@gmail.com>
2026-05-26 11:02:42 +0800

    add new photos

--- src/data/photos.ts
+++ src/data/photos.ts
@@ -40,4 +40,34 @@ export const photos: Photo[] = [
     src: "https://os84zrjblr.ufs.sh/f/A3OWyvtOmv8nWYjl3NSU1nK4oOMC0cazH8qyfSbV7h3gExB5",
     alt: "配件图细节2",
   },
+  {
+    id: "8",
+    src: "https://os84zrjblr.ufs.sh/f/A3OWyvtOmv8nAhDv61tOmv8nw3jxFY0i4PSNrVytgQB9KURD",
+    alt: "牛牛钥匙扣，可晃动",
+  },
+  {
+    id: "9",
+    src: "https://os84zrjblr.ufs.sh/f/A3OWyvtOmv8n7Rf7Bapn5Jt6Epgw89zD0j7TQ2ha1Xr4MyZH",
+    alt: "机车男正面",
+  },
+  {
+    id: "10",
+    src: "https://os84zrjblr.ufs.sh/f/A3OWyvtOmv8n3TX9ihKGTK7dgnB8yJtOAUoMb1v6FPakS0cR",
+    alt: "上色版脂包肌叉腰正面",
+  },
+  {
+    id: "11",
+    src: "https://os84zrjblr.ufs.sh/f/A3OWyvtOmv8nnQuap4ISiApaUVLIoEM562ermFNWjDYhRKzc",
+    alt: "上色版脂包肌叉腰正面仰视",
+  },
+  {
+    id: "12",
+    src: "https://os84zrjblr.ufs.sh/f/A3OWyvtOmv8nqrPELZzxBQHDdSrbRYmLva2XAK9zPeFOU47V",
+    alt: "上色版脂包肌叉腰背面",
+  },
+  {
+    id: "13",
+    src: "https://os84zrjblr.ufs.sh/f/A3OWyvtOmv8nOcmzaQA1KaEGOwJjhU9iPmzu5ZcnyHlN4vrg",
+    alt: "上色版脂包肌叉腰侧面",
+  },
 ];
